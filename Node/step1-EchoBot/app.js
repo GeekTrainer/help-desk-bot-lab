06abd9b857409b39fa88c34ecdf51afb5e088f80
Emanuel Vecchio <vecchioemanuel@gmail.com>
2017-05-03 12:15:41 -0300

    Updated step1 - improved comments

--- Node/step1-EchoBot/app.js
+++ Node/step1-EchoBot/app.js
@@ -4,23 +4,23 @@ const builder = require('botbuilder');
 
 const app = express();
 
-// Setup Express Server
+// Setup Express server
 app.listen(process.env.port || process.env.PORT || 3978, '::', () => {
     console.log('Server Up');
 });
 
-// Create connector
+// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-// Expose connector
+// Listen for messages from users
 app.post('/api/messages', connector.listen());
 
-// Create Chat Bot
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, [
-    (session, args, next) => {
+    function (session, args, next) {
         session.send('You said: ' + session.message.text);
     }
 ]);
